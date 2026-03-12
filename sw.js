3d5adcd3562bf2487474292b459974a04763af7c
Xicheng Xie <xichengxie@Xichengs-Mac-mini.local>
2026-03-12 15:55:16 -0500

    Fix mobile date/done spacing gap

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'workout-tracker-v8';
+const CACHE = 'workout-tracker-v9';
 const ASSETS = ['./', './index.html', './styles.css', './app.js', './manifest.json'];
 
 self.addEventListener('install', (event) => {
